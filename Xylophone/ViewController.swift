c49033d60d1e23c7d06d3b6e05492f65892aedae
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-14 15:13:40 +0200

    multiple btns, same IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,39 +8,24 @@
 
 import UIKit
 import AVFoundation
+
 class ViewController: UIViewController {
     
-    var player : AVAudioPlayer?
-    
+    var player: AVAudioPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // UIButton
+    //All btns connencted to same IBAction function
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
     }
     
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+            
     }
 }
-
