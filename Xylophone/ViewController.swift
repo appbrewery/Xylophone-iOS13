3b0c0217c6114038e1e4dba8a17b1723a097abe9
YashGupta018 <ygupta2001@gmail.com>
2022-11-25 15:23:09 +0530

    added a feature where the button dims when pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(soundName: sender.currentTitle!)
+        print("Start")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            print("End")
+        }
         
     }
     
