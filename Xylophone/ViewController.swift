e62ecc55736efec3a32fddb3ede2198ad5301b36
Prasshant Shanmugalingam <stickoking89@icloud.com>
2023-07-11 14:18:27 +0800

    Implement PlaySound Dynamic Function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    // A property to store the audio player
+    var audioPlayer: AVAudioPlayer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("I got pressed")
+        playSound(fileName: "C")
+    }
+    
+    // A function that takes a sound file name as a parameter and plays the sound using AVAudioPlayer
+    func playSound(fileName: String) {
+      // Load the sound file from the app bundle with extension "wav"
+      if let soundURL = Bundle.main.url(forResource: fileName, withExtension: "wav") {
+        do {
+          // Initialize the audio player with the sound file URL
+          audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+          // Set the delegate to self if you want to implement any of the AVAudioPlayerDelegate methods
+          audioPlayer?.delegate = self
+          // Prepare to play
+          audioPlayer?.prepareToPlay()
+          // Play the sound
+          audioPlayer?.play()
+        } catch {
+          // Handle any errors
+          print(error.localizedDescription)
+        }
+      }
+    }
 
     
 
